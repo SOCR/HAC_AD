a98b1145d40ba438ff4bd172f1965fc1961d286e
CooperStansbury <cstansbu@umich.edu>
2019-06-19 13:54:45 -0400

    rename card_inputs dir to make it more clear

--- code/test_jar.sh
+++ code/test_jar.sh
@@ -1,5 +1,5 @@
 echo "the pt walked along the riverbed. For three h they did things." | java -jar TightSenseClustering.jar \
-    card_resources/word.txt \
-    card_resources/abbr.txt \
-    card_resources/profile/ \
-    card_resources/VABBR_DS_beta.txt.add_semantic_type
+    card_inputs/word.txt \
+    card_inputs/abbr.txt \
+    card_inputs/profile/ \
+    card_inputs/VABBR_DS_beta.txt.add_semantic_type

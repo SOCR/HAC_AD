44bff9ffebd61f7a92f8d6b5b2973a764460089c
CooperStansbury <cstansbu@umich.edu>
2019-06-20 12:34:26 -0400

    update function documentation, fix labs load, continued work on descriptive statistics

--- code/load_and_convert_functions.R
+++ code/load_and_convert_functions.R
@@ -2,10 +2,13 @@
 # load and convert is a series of functions for import into diiferent work spaces
 # @Author: Cooper Stansbury
 
+print(paste0("Source 'load_and_convert_functions.R' Successful: ", Sys.time()))
+
 
 check_when_save <- function(filepath){
-  # 'function to see if the object can be saved to specified location
-  # '@filepath: a string input. Should contain the file extension
+  #'  function to see if the object can be saved to specified location
+  #'  @param filepath: a string input. Should contain the file extension
+  
   if (file.exists(filepath)) {
     print(paste("WARNING:'", normalizePath(filepath),
                 "'already exists. File not saved. Please check your work."))
@@ -18,10 +21,11 @@ check_when_save <- function(filepath){
 
 
 check_column_names <- function(df, replace=TRUE) {
-  # 'function to handle column names the contain special characters
-  # '@df: data frame to check for improperly formatted column names
-  # '@replace: replace = TRUE will return a new data frame with special
+  #' function to handle column names the contain special characters
+  #' @param df: data frame to check for improperly formatted column names
+  #' @param replace: replace = TRUE will return a new data frame with special
   # characters removed, FALSE will only print out problematic column names
+  
   temp_df <- df
   for (column_name in colnames(df)) {
     only_underscores <- !grepl("[^0-9A-Za-z_///' ]", column_name)
@@ -49,23 +53,38 @@ check_column_names <- function(df, replace=TRUE) {
 }
 
 
+print_dimensionality <- function(df){
+  #' function to print the number of rows and columns for a given data.frame
+  #' @param df: the input data.frame
+  
+  print(paste('Input object n rows:', toString(nrow(df))))
+  print(paste('Input object n columns:', toString(ncol(df))))
+}
+
+
 use_first_row_as_col_names <- function(df) {
-  # 'function that uses the first row of a data frame as column names
-  # 'returns new data frame
-  # '@df: data.frame
+  #' function that uses the first row of a data frame as column names
+  #' returns new data frame
+  #' @param df: data.frame
+  
   names(df) <- as.character(unlist(df[1,]))
   return(df[-1,])
 }
 
 
 read_multi_character_delim <- function(filepath, delimiter, header=TRUE) {
-  # 'function to read file delimited with a multi-character separator
-  # '@filepath: string corresponding to the realtive fil path containing the data
-  # '@delimiter: string with the separator
+  #' function to read file delimited with a multi-character separator
+  #' @param filepath: string corresponding to the realtive fil path containing the data
+  #' @param delimiter: string with the separator
+  
+  start.time <- Sys.time() # get seconds to benchmark performance
+  
+  #' @NOTE: open connection 
   file_connection <- file(filepath)
   data <- readLines(file_connection)
   close(file_connection)
 
+  #' @NOTE: split each record
   records <- sapply(data, strsplit, split=delimiter)
   tmp_df <- data.frame(t(sapply(records,c)))
   rownames(tmp_df) <- 1: nrow(tmp_df)
@@ -73,8 +92,14 @@ read_multi_character_delim <- function(filepath, delimiter, header=TRUE) {
 
   if (header) {
     df <- use_first_row_as_col_names(df)
+    print_dimensionality(df)
+    end.time <- Sys.time()
+    print(paste("Execution time:", end.time - start.time))
     return(df)
   } else {
+    print_dimensionality(df)
+    end.time <- Sys.time()
+    print(paste("Execution time:", end.time - start.time))
     return(df)
   }
 }
@@ -84,20 +109,20 @@ read_multi_character_delim_fast <- function(filepath,
                                             delimiter,
                                             maxLength=20000000,
                                             v=TRUE, header=TRUE) {
-  
-  # 'function to read file delimited with a multi-character separator
-  # '@filepath: string corresponding to the realtive fil path containing the data
-  # '@delimiter: string with the separator
-  # '@maxLength: need to guess at number or rows (which can sometimes be obtained 
+  #' function to read file delimited with a multi-character separator
+  #' @param filepath: string corresponding to the realtive fil path containing the data
+  #' @param delimiter: string with the separator
+  #' @param maxLength: need to guess at number or rows (which can sometimes be obtained 
   #  using 'wc -l [file]) in order for fread() to sample correctly. Default is set
   #  to over-estimate our largest file.
-  # '@v:verbose: passed to fread()
-  # '@header: boolen that calls use_first_row_as_col_names().
+  #' @param v (verbose): passed to fread()
+  #' @param header: boolen that calls use_first_row_as_col_names().
   # this improves upon the example above by using data.table and fread
   # See: https://stackoverflow.com/questions/1727772
   
+  start.time <- Sys.time()
   library(data.table)
-  gc() # careful with memory
+  gc() # clean-up
   tmp_tbl <- fread(filepath,
                    sep=NULL,
                    header=FALSE,
@@ -116,9 +141,15 @@ read_multi_character_delim_fast <- function(filepath,
 
   if (header) {
     df <- use_first_row_as_col_names(df)
+    print_dimensionality(df)
+    end.time <- Sys.time()
+    print(paste("Execution time:", end.time - start.time))
     return(df)
     gc()
   } else {
+    print_dimensionality(df)
+    end.time <- Sys.time()
+    print(paste("Execution time:", end.time - start.time))
     return(df)
     gc()
   }
@@ -130,21 +161,21 @@ read_notes_custom_delim <- function(filepath,
                                     maxLength=20000000,
                                     skip=FALSE,
                                     v=TRUE, header=TRUE) {
-  
-  # 'function to read the NOTES file delimited with a multi-character separator
-  # '@filepath: string corresponding to the realtive fil path containing the data
-  # '@delimiter: string with the separator
-  # '@maxLength: need to guess at number or rows (which can sometimes be obtained 
+  #' function to read the NOTES file delimited with a multi-character separator
+  #' @param filepath: string corresponding to the realtive fil path containing the data
+  #' @param delimiter: string with the separator
+  #' @param maxLength: need to guess at number or rows (which can sometimes be obtained 
   #  using 'wc -l [file]) in order for fread() to sample correctly. Default is set
   #  to over-estimate our largest file.
-  # '@v:verbose: passed to fread()
-  # '@header: boolen that calls use_first_row_as_col_names().
+  #' @param v (verbose): passed to fread()
+  #' @param header: boolen that calls use_first_row_as_col_names().
   # this improves upon the example above by using data.table and fread
   # See: https://stackoverflow.com/questions/1727772
   
+  start.time <- Sys.time()
   library(data.table)
   library(readr)
-  gc() # careful with memory
+  gc() # clean-up
   
   # can't use fread because comment.char() note implemented:
   # https://github.com/Rdatatable/data.table/issues/856
@@ -166,38 +197,60 @@ read_notes_custom_delim <- function(filepath,
   
   if (header) {
     df <- use_first_row_as_col_names(df)
+    print_dimensionality(df)
+    end.time <- Sys.time()
+    print(paste("Execution time:", end.time - start.time))
     return(df)
     gc()
   } else {
+    print_dimensionality(df)
+    end.time <- Sys.time()
+    print(paste("Execution time:", end.time - start.time))
     return(df)
     gc()
   }
 }
 
 
-read_csv_from_path <- function(filepath,
-                               header=TRUE,
-                               maxLength=2000000) {
-  # 'function to read a csv file from a relative filepath and return a new data.frame
-  # '@filepath string corresponding to the relative filepath
-  # '@maxLength: need to guess at number or rows (which can sometimes be obtained 
-  #  using 'wc -l [file]) in order for fread() to sample correctly. Default is set
-  #  to over-estimate our largest file.
-  # '@header: boolen passed to read.table
+get_first_n_splits <- function(string, pattern, n) {
+  #' function to split a st ring on the first n occurences of a given input
+  #' @param string: the string value split (expected to be passed via 'apply')
+  #' @param pattern: the string value to split on
+  #' @param n: the number of occurences of the the 'pattern' to split before stopping
+  
+  return(unlist(strsplit(string, pattern))[1:n])
+}
 
+
+
+read_labs <- function(filepath) {
+  #' function to read the labs.dsv into a new data.frame
+  #' this function is intended ONLY for the labs file
+  #' @param filepath string corresponding to the relative filepath
+  
+  start.time <- Sys.time()
+  library(readr)
   library(data.table)
-  data <-  read.table(file=filepath,
-                header=header,
-                nrows = maxLength)
-  print(paste('Input object n rows:', toString(nrow(data))))
-  print(paste('Input object n columns:', toString(nrow(data))))
-  return(data)
+  gc()
+  tmp_tbl <- read_table(filepath,
+                        col_names = FALSE,
+                        progress=TRUE)
+  #' @NOTE: these parameters are hard-coded specifically for this file
+  tmp_tbl <- as.data.frame(t(sapply(labs_df$X1, get_first_n_splits, pattern=",", n=20)))
+  rownames(tmp_tbl) <- NULL
+  df <- use_first_row_as_col_names(test)
+  print_dimensionality(df)
+  end.time <- Sys.time()
+  print(paste("Execution time:", end.time - start.time))
+  return(df)
 }
 
 
 read_xlsx_from_path <- function(filepath) {
-  # 'function to read an xlsx file from a relative filepath and return a new data.frame
-  # '@filepath string corresponding to the relative filepath
+  #' function to read an xlsx file from a relative filepath and return a new data.frame
+  #' @param filepath string corresponding to the relative filepath
+  
+  start.time <- Sys.time()
   if(!file.exists(filepath)) {
     print('ERROR: filepath does not exist.')
     print('Please make sure you are inputting a valid filepath realtive to: ')
@@ -212,19 +265,21 @@ read_xlsx_from_path <- function(filepath) {
             return(NA)
         }
       )
-      data <- as.data.frame(read_excel(filepath))
-      print(paste('Input object n rows:', toString(nrow(data))))
-      print(paste('Input object n columns:', toString(ncol(data))))
-      return(data)
+      df <- as.data.frame(read_excel(filepath))
+      print_dimensionality(df)
+      end.time <- Sys.time()
+      print(paste("Execution time:", end.time - start.time))
+      return(df)
   }
 }
 
 
 save_rData <- function(rObject, dir, filename) {
-  # 'function to save an RData object
-  # '@rObject: object to save
-  # '@dir: directory, including slash
-  # '@filename: a name for the file, will be automatically appeneded with current date
+  #' function to save an RData object
+  #' @param rObject: object to save
+  #' @param dir: directory, including slash
+  #' @param filename: a name for the file, will be automatically appeneded with current date
+  
   today_date = format(Sys.time(), "%m%d%Y")
   save_path = paste(dir, today_date, "-", filename , sep="")
   check_when_save(save_path)
@@ -233,10 +288,11 @@ save_rData <- function(rObject, dir, filename) {
 
 
 save_csv <- function(rObject, dir, filename) {
-  # 'function to save a csv file
-  # '@rObject: object to save
-  # '@dir: directory, including slash
-  # '@filename: a name for the file, will be automatically appeneded with current date
+  #' function to save a csv file
+  #' @param rObject: object to save
+  #' @param dir: directory, including slash
+  #' @param filename: a name for the file, will be automatically appeneded with current date
+  
   today_date = format(Sys.time(), "%m%d%Y")
   save_path = paste(dir, today_date, "-", filename , sep="")
   check_when_save(save_path)
